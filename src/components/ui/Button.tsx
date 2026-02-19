4a933e3a79638a88581c6ed03376ebd98a1f6b61
Shaan2170 <shaanmustafa1234@gmail.com>
2026-02-19 13:07:04 +0530

    Added Buttons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,15 +1,31 @@
 
-export interface ButtonProps {
-    variant: "primary" | "secondary";
+type Variants = "primary" | "secondary"
+interface ButtonProps {
+    variant: Variants;
     size: "sm" | "md" | "lg";
     text: string;
-    startIcon: any;
-    endIconL: any;
+    startIcon?: any;
+    endIcon?: any;
     onClick: () => void;
 }
 
-export const Button = (props: ButtonProps ) => {
-    return <button></button>
+const variantStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-400 text-purple-600",
 }
 
-<Button variant ="primary"  size="md" onClick={} text ={}></Button>
+const sizeStyles = {
+    "sm": "py-1 px-2",
+    "md": "py-2 px-4",
+    "lg": "py-4 px-6"
+}
+
+const defaultStyles = "rounded-md inline-flex"
+
+//
+export const Button = (props: ButtonProps) => {
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>
+        {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null} {props.text} {props.endIcon}</button>
+}
+
+<Button variant="primary" size="md" onClick={() => {}} text={"asd"} />
